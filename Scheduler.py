1b5470587a85a4c1f62337314c5c042029cd2e1b
Bro4all <omarbro4all@gmail.com>
2019-04-24 12:58:12 -0700

    Reading from file

--- Scheduler.py
+++ Scheduler.py
@@ -2,17 +2,25 @@ import numpy as np
 
 def readfile():
     job_file = open("jobs.dat", "r")
-    job = [[]]
+    job = []
     x = 0
     line = job_file.readline()
     while line:
-        job_attributes = line.split()
-        job_attributes = np.asarray(job_attributes)
-        print(job_attributes)
-        for i in range(len(job_attributes)):
-            job[x][i].append(job_attributes)
-        x = x + 1
+        line=line.split()
+        job.append(Job(line[0],line[1],line[2]))
         line = job_file.readline()
+    print(job[0].job_id)
+
+    
+
+class Job:
+    def __init__(self, job_id, arrival_time, duration):
+        self.job_id = job_id
+        self.arrival_time = arrival_time
+        self.duration = duration
+
+
+
 
 
 if __name__ == "__main__":
