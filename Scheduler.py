24f55ffc98ee745d2178efda3a82f044884750e4
Bro4all <omarbro4all@gmail.com>
2019-04-24 13:07:14 -0700

    fifo

--- Scheduler.py
+++ Scheduler.py
@@ -1,17 +1,31 @@
-import numpy as np
+
 
 def readfile():
     job_file = open("jobs.dat", "r")
-    job = []
-    x = 0
+    jobs = []
     line = job_file.readline()
     while line:
         line=line.split()
-        job.append(Job(line[0],line[1],line[2]))
+        jobs.append(Job(line[0], line[1], line[2]))
         line = job_file.readline()
-    print(job[0].job_id)
+    print(jobs[0].job_id)
 
-    
+
+def fifo(jobs):
+    pass
+
+
+def sjf(jobs):
+    pass
+
+
+def bjf(jobs):
+    pass
+
+
+def stcf(jobs):
+    pass
+
 
 class Job:
     def __init__(self, job_id, arrival_time, duration):
@@ -19,8 +33,10 @@ class Job:
         self.arrival_time = arrival_time
         self.duration = duration
 
-
-
+        self.start = None
+        self.completion = None
+        self.response_time = None
+        self.turn_around = None
 
 
 if __name__ == "__main__":
